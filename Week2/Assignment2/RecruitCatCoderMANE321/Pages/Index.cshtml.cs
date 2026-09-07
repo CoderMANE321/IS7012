f2df11084349fe5f5a43def74d5fec5579f30a95
CODERMANE321 <deontamwilliams@gmail.com>
2026-09-07 15:07:22 -0400

    Assignment2

---
+++ Week2/Assignment2/RecruitCatCoderMANE321/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatCoderMANE321.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
