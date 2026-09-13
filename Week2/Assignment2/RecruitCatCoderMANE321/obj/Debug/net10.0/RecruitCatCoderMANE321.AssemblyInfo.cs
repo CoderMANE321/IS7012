55561326d0ac1209593905d52d1c256a8779fddf
CODERMANE321 <Deontamwilliams@gmail.com>
2026-09-13 14:16:44 -0400

    Merge branch 'main' of github.com:CoderMANE321/IS7012

--- Week2/Assignment2/RecruitCatCoderMANE321/obj/Debug/net10.0/RecruitCatCoderMANE321.AssemblyInfo.cs
+++ Week2/Assignment2/RecruitCatCoderMANE321/obj/Debug/net10.0/RecruitCatCoderMANE321.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitCatCoderMANE321")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9a4c44df61d097e5fa2d93637633e3eecb9bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659ce470ea0a4f68c826ea1ebf0fceccfa7d64f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitCatCoderMANE321")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitCatCoderMANE321")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
