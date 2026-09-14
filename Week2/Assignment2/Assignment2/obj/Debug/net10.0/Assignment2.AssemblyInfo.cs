03d3c826b8a7229fb8cdaf6b2f03d565a8c58299
CODERMANE321 <Deontamwilliams@gmail.com>
2026-09-13 20:48:26 -0400

    check constraints on models

--- Week2/Assignment2/Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
+++ Week2/Assignment2/Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659ce470ea0a4f68c826ea1ebf0fceccfa7d64f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1cc62cd7ddb7b2c5bad5d05709a8f50aee62a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
