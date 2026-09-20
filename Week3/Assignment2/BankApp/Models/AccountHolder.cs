43da25e60469a67e0f6f4d59ad33bbd3c4bb5da7
CODERMANE321 <Deontamwilliams@gmail.com>
2026-09-20 14:09:23 -0400

    save before model change

--- Week3/Assignment2/BankApp/Models/AccountHolder.cs
+++ Week3/Assignment2/BankApp/Models/AccountHolder.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel;
 public class AccountHolder
 {
     public int AccountHolderId { get; set; }
 
+    [DisplayName("First Name")]
     public string FirstName { get; set; } = string.Empty;
 
+    [DisplayName("Last Name")]
     public string LastName { get; set; } = string.Empty;
 
+    public string FullName
+    {
+        get { return $"{FirstName} {LastName}"; }
+    }
+
+    [DisplayName("Email")]
     public string Email { get; set; } = string.Empty;
 
     public List<AccountHolderAccount> BankAccounts { get; set; } = new();
--- Week3/Assignment2/BankApp/obj/Debug/net10.0/BankApp.AssemblyInfo.cs
+++ Week3/Assignment2/BankApp/obj/Debug/net10.0/BankApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1cc62cd7ddb7b2c5bad5d05709a8f50aee62a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d3c826b8a7229fb8cdaf6b2f03d565a8c58299")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
